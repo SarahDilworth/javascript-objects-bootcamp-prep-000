21dc0649eeab155364d4cc6305a84d7a6cd843c0
Sarah Dilworth <sarah.petty18@gmail.com>
2018-06-01 12:52:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { 'Slowdive' : 'Alison', 'My Bloody Valentine' : 'Sometimes'};
 
 function updatePlaylist(playlist) {
-return Object.assign({}, playkist, {['Phil Ochs'] : "Here\'s "
+return Object.assign({}, playkist, {['Phil Ochs'] : "Here\'s to the State of Mississippi" });
 }
+
